072ea3b6d0c693a8fe6a83d015bba60230b95490
Ekaterina Chumak <Osa88@list.ru>
2025-07-06 23:22:33 +0300

    feat: add starts in javascript

---
+++ scriptstars.js
@@ -0,0 +1,166 @@
+// sertificate 
+gsap.to("#certificate", {
+  text: "Certificates",
+  duration: 4,
+  repeat: 2,
+})
+gsap.to("#resume", {
+  text: "Resume",
+  duration: 4,
+  repeat: 2,
+})
+gsap.to("#certificateRu", {
+  text: "Сертификаты",
+  duration: 4,
+  repeat: 2,
+})
+gsap.to("#resumeRu", {
+  text: "Резюме",
+  duration: 4,
+  repeat: 2,
+})
+
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 250,
+        "density": {
+          "enable": true,
+          "value_area": 600
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+
+
+
+  // title Text
+  const text = document.querySelector('.text');
+
+const splitText = (el) => {
+	el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
+  return `<div class="word">` +
+			m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
+			`</div>`;
+	});
+	return el;
+};
+
+const split = splitText(text);
+
+function random(min, max){
+  return (Math.random() * (max - min)) + min;
+}
+
+Array.from(split.querySelectorAll('.letter')).forEach((el, idx) => {
+	TweenMax.from(el, 2.5, {
+		opacity: 0,
+		scale: .1,
+		x: random(-500, 500),
+		y: random(-500, 500),
+		z: random(-500, 500),
+		delay: idx * 0.02,
+		repeat: 0,
+	})
+});
